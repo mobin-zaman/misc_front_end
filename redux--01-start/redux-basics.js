665a1618ae5343210fa3a0196ee8772c2f9b2982
mobin-zaman <mobinzamandev@gmail.com>
2020-04-27 21:34:01 +0600

    huddai redux

---
+++ redux--01-start/redux-basics.js
@@ -0,0 +1,42 @@
+const redux = require("redux");
+
+const initialState = {
+  counter: 0,
+};
+
+const createStore = redux.createStore;
+
+//Reducer
+const rootReducer = (state = initialState, action) => {
+  if (action.type == "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type == "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value,
+    };
+  }
+
+  return state;
+};
+//Store
+const store = createStore(rootReducer);
+
+console.log(store.getState());
+
+//Subscription
+store.subscribe(() => {
+  console.log("[Subscription]", store.getState());
+});
+
+//Dispatching action
+
+store.dispatch({ type: "INC_COUNTER" }); //all uppercase and being descriptive is the convention
+store.dispatch({ type: "ADD_COUNTER", value: 10 });
+
+console.log(store.getState());
